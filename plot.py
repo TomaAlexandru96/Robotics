3e9e947975499883f9c3a54d9a7c3bacaf21c767
Alexandru Toma <alexandru.toma15@imperial.ac.uk>
2017-10-22 15:36:44 +0100

    Added logfiles for first calibration

--- plot.py
+++ plot.py
@@ -1,6 +1,18 @@
 import matplotlib.pyplot as plt
+import sys
 
-with open("logfile.txt") as f:
+logfile = "logfile.txt"
+diff = False
+
+if len(sys.argv) > 1:
+    for arg in sys.argv:
+        arg = arg.split('=')
+        if arg[0] == 'd':
+            diff = arg[1]
+        elif arg[0] == 'f':
+            logfile = arg[1]
+
+with open(logfile) as f:
     read_data = f.readlines()
 
 time = []
@@ -18,15 +30,29 @@ for line in read_data:
     reference_angle1.append(line.split()[3])
     angle1.append(line.split()[4])
 
-plt.hold(True)
-plt.plot(time, reference_angle0, label="Reference Angle 0", color="green")
-plt.plot(time, angle0, label="Angle 0", color="blue")
-plt.plot(time, reference_angle1, label="Reference Angle 1", color="red")
-plt.plot(time, angle1, label="Angle 1", color="purple")
-
-plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0)
-
-plt.title("Calibration")
-plt.xlabel("Time")
-
+if not diff:
+    plt.hold(True)
+    plt.plot(time, reference_angle0, label="Reference Angle 0", color="green")
+    plt.plot(time, angle0, label="Angle 0", color="blue")
+    plt.plot(time, reference_angle1, label="Reference Angle 1", color="red")
+    plt.plot(time, angle1, label="Angle 1", color="purple")
+    
+    plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0)
+    
+    plt.title("Calibration")
+    plt.xlabel("Time")
+else:
+    diff0 = []
+    diff1 = []
+    for i in range(0, len(reference_angle0)):
+        diff0.append(float(reference_angle0[i]) - float(angle0[i]))
+        diff1.append(float(reference_angle1[i]) - float(angle1[i]))
+    plt.hold(True)
+    plt.plot(time, diff0, label="Angle Diff 0", color="blue")
+    plt.plot(time, diff1, label="Angle Diff 1", color="red")
+    
+    plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0)
+    
+    plt.title("Calibration Differential")
+    plt.xlabel("Time")
 plt.show()
