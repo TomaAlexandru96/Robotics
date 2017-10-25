4ba032439a22fdde0f3c8a5cde92617064d58862
VladH <vladhadarean7@gmail.com>
2017-10-25 13:10:42 +0000

    Final parameters and functions for movement

--- mainStart.py
+++ mainStart.py
@@ -13,44 +13,83 @@ interface.motorEnable(motors[0])
 interface.motorEnable(motors[1])
 
 motorParams0 = interface.MotorAngleControllerParameters()
-motorParams0.maxRotationAcceleration = 5.0
-motorParams0.maxRotationSpeed = 11.0
-motorParams0.feedForwardGain = 13.45
+motorParams0.maxRotationAcceleration = 4.0
+motorParams0.maxRotationSpeed = 10.0
+motorParams0.feedForwardGain = 13.17
 motorParams0.minPWM = 25
 motorParams0.pidParameters.minOutput = -255
 motorParams0.pidParameters.maxOutput = 255
-motorParams0.pidParameters.k_p = 670.0
-motorParams0.pidParameters.k_i = 200.0
-motorParams0.pidParameters.k_d = 0.0
+motorParams0.pidParameters.k_p = 600.0
+motorParams0.pidParameters.k_i = 230.0
+motorParams0.pidParameters.k_d = 50.0
 
 motorParams1 = interface.MotorAngleControllerParameters()
-motorParams1.maxRotationAcceleration = 5.0
-motorParams1.maxRotationSpeed = 11.0
-motorParams1.feedForwardGain = 13.3
+motorParams1.maxRotationAcceleration = 4.0
+motorParams1.maxRotationSpeed = 10.0
+motorParams1.feedForwardGain = 13.12
 motorParams1.minPWM = 25
 motorParams1.pidParameters.minOutput = -255
 motorParams1.pidParameters.maxOutput = 255
-motorParams1.pidParameters.k_p = 730.0
-motorParams1.pidParameters.k_i = 160.0
-motorParams1.pidParameters.k_d = 0.0
+motorParams1.pidParameters.k_p = 600.0
+motorParams1.pidParameters.k_i = 230.0
+motorParams1.pidParameters.k_d = 50.0
 
 interface.setMotorAngleControllerParameters(motors[0], motorParams0)
 interface.setMotorAngleControllerParameters(motors[1], motorParams1)
 
-interface.startLogging("logfile.txt")
-while True:
-	angle0 = float(input("Enter a angle to rotate 0 (in radians): "))
-        angle1 = float(input("Enter a angle to rotate 1 (in radians): "))
+angle90=3.2
+length40=11.6
 
-	interface.increaseMotorAngleReferences(motors,[angle0,angle1])
-
-	while not interface.motorAngleReferencesReached(motors) :
+def goStraight40cm():
+	interface.increaseMotorAngleReferences(motors,[length40,length40])
+	while not interface.motorAngleReferencesReached(motors):
 		motorAngles = interface.getMotorAngles(motors)
 		if motorAngles :
 			print "Motor angles: ", motorAngles[0][0], ", ", motorAngles[1][0]
 		time.sleep(0.1)
+	return
 
-	print "Destination reached!"
+def turnLeft90():
+	interface.increaseMotorAngleReferences(motors,[-angle90,angle90])
+	while not interface.motorAngleReferencesReached(motors):
+		motorAngles = interface.getMotorAngles(motors)
+		if motorAngles :
+			print "Motor angles: ", motorAngles[0][0], ", ", motorAngles[1][0]
+		time.sleep(0.1)	
+	return
+
+def turnRight90():
+	interface.increaseMotorAngleReferences(motors,[angle90,-angle90])
+	while not interface.motorAngleReferencesReached(motors):
+		motorAngles = interface.getMotorAngles(motors)
+		if motorAngles :
+			print "Motor angles: ", motorAngles[0][0], ", ", motorAngles[1][0]
+		time.sleep(0.1)	
+	return
+		
+
+interface.startLogging("logfile.txt")
+while True:
+        square = bool(input("Do you want to draw a square?: "))
+
+        if square:
+		for x in range(3):
+			goStraight40cm()
+			turnLeft90()
+		goStraight40cm()
+	else:
+		angle0 = float(input("Enter a angle to rotate 0 (in radians): "))
+        	angle1 = float(input("Enter a angle to rotate 1 (in radians): "))
+
+		interface.increaseMotorAngleReferences(motors,[angle0,angle1])
+
+		while not interface.motorAngleReferencesReached(motors) :
+			motorAngles = interface.getMotorAngles(motors)
+			if motorAngles :
+				print "Motor angles: ", motorAngles[0][0], ", ", motorAngles[1][0]
+			time.sleep(0.1)
+
+		print "Destination reached!"
 	
 
 interface.stopLogging()
