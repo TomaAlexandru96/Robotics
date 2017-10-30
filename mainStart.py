7489bfe509825ed73653dddc0a413840aa877fcb
Norbert Podsadowski <norbo11@googlemail.com>
2017-10-30 13:56:56 +0000

    More refactoring

--- mainStart.py
+++ mainStart.py
@@ -1,53 +1,22 @@
 import brickpi
 import time
+import robotConfig
 
-interface=brickpi.Interface()
-interface.initialize()
+interface = brickpi.Interface()
 
-right = 3
-left = 1
-
-motors = [left, right]
-
-interface.motorEnable(motors[0])
-interface.motorEnable(motors[1])
-
-motorParams0 = interface.MotorAngleControllerParameters()
-motorParams0.maxRotationAcceleration = 4.0
-motorParams0.maxRotationSpeed = 10.0
-motorParams0.feedForwardGain = 13.17
-motorParams0.minPWM = 25
-motorParams0.pidParameters.minOutput = -255
-motorParams0.pidParameters.maxOutput = 255
-motorParams0.pidParameters.k_p = 570.0
-motorParams0.pidParameters.k_i = 195.0
-motorParams0.pidParameters.k_d = 30.0
-
-motorParams1 = interface.MotorAngleControllerParameters()
-motorParams1.maxRotationAcceleration = 4.0
-motorParams1.maxRotationSpeed = 10.0
-motorParams1.feedForwardGain = 13.12
-motorParams1.minPWM = 25
-motorParams1.pidParameters.minOutput = -255
-motorParams1.pidParameters.maxOutput = 255
-motorParams1.pidParameters.k_p = 570.0
-motorParams1.pidParameters.k_i = 195.0
-motorParams1.pidParameters.k_d = 30.0
-
-interface.setMotorAngleControllerParameters(motors[0], motorParams0)
-interface.setMotorAngleControllerParameters(motors[1], motorParams1)
-
-angle90=3.2
-length40=11.55
+angle90 = 3.2
+length40 = 11.55
 
 def driveUntilReferenceAnglesReached(angles):
-    interface.increaseMotorAngleReferences(motors,angles)
-    while not interface.motorAngleReferencesReached(motors):
-            motorAngles = interface.getMotorAngles(motors)
-            if motorAngles :
-                    print "Motor angles: ", motorAngles[0][0], ", ", motorAngles[1][0]
-            time.sleep(0.1)
-    return
+    interface.increaseMotorAngleReferences(robotConfig.motors, angles)
+
+    while not interface.motorAngleReferencesReached(robotConfig.motors):
+        motorAngles = interface.getMotorAngles(robotConfig.motors)
+
+        if motorAngles:
+            print("Motor angles: ", motorAngles[0][0], ", ", motorAngles[1][0])
+
+        time.sleep(0.1)
 
 def askForReferenceAngles():
     angle0 = float(input("Enter a angle to rotate 0 (in radians): "))
@@ -55,7 +24,7 @@ def askForReferenceAngles():
 
     driveUntilReferenceAnglesReached([angle0,angle1])
 
-    print "Destination reached!"
+    print("Destination reached!")
 
 def goStraight40cm():
     driveUntilReferenceAnglesReached([length40,length40])
@@ -65,17 +34,21 @@ def turnLeft90():
 
 def turnRight90():
     driveUntilReferenceAnglesReached([angle90,-angle90])
-		
+
 def drawSquare():
     for x in range(3):
             goStraight40cm()
             turnLeft90()
     goStraight40cm()
 
-# interface.startLogging("logfile.txt")
+def main():
+    interface.initialize()
+    # interface.startLogging("logfile.txt")
 
+    robotConfig.configureRobot(interface)
 
+    # interface.stopLogging()
+    interface.terminate()
 
-# interface.stopLogging()
-
-interface.terminate()
+if __name__ == "__main__":
+    main()
