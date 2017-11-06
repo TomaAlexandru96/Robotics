0d275641a97b5a3d8866b55f3e6a86f1e05a9cfc
Alexandru Toma <alexandru.toma15@imperial.ac.uk>
2017-11-06 14:06:00 +0000

    refactored waypount

--- robot.py
+++ robot.py
@@ -2,6 +2,7 @@ import brickpi
 import time
 import robotConfig
 import sys
+import math
 
 interface = brickpi.Interface()
 
@@ -57,15 +58,40 @@ def drawSquare():
     goStraight40cm()
 
 
-def initInterfaceAndRun(func, withLog = False):
+def initInterface(withLog = False):
     interface.initialize()
     if withLog:
         interface.startLogging("logfile.txt")
     robotConfig.configureRobot(interface)
-    func()
+
+
+def stopInterface(withLog = False):
     if withLog:
         interface.stopLogging()
     interface.terminate()
+
+
+robotPosition = [0, 0, 0]
+
+
+def navigateToWaypoint(x, y):
+    dx = x - robotPosition[0]
+    dy = y - robotPosition[1]
+    alpha = math.atan2(dy, dx)
+
+    beta = alpha - robotPosition[2]
+
+    if beta <= -math.pi:
+        beta += 2*math.pi
+    elif beta > math.pi:
+        beta -= 2*math.pi
+
+    rotate(beta)
+    goStraight(math.sqrt(dx * dx + dy * dy))
+
+    robotPosition[0] = x
+    robotPosition[1] = y
+    robotPosition[2] = alpha
 
 
 def run():
@@ -77,10 +103,17 @@ def run():
         turnRight90()
     elif sys.argv[1] == "4":
         drawSquare()
+    elif sys.argv[1] == "5":
+        while True:
+            x = float(input("x="))
+            y = float(input("y="))
+            navigateToWaypoint(x, y)
 
 
 def main():
-    initInterfaceAndRun(run)
+    initInterface()
+    run()
+    stopInterface()
 
 
 if __name__ == "__main__":
