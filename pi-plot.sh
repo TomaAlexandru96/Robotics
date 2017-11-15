2e00577ccb90767a445fa49c49bd15c7582750dd
VladH <vladhadarean7@gmail.com>
2017-11-15 13:12:29 +0000

    Fixed repo

--- pi-plot.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-scp pi@129.31.224.44:~/Robotics/logfile.txt . 
-# python plot.py & 
-python plot.py &
-
--- robotConfig.py
+++ robotConfig.py
@@ -5,57 +5,28 @@ touch_sensor_right = 5
 
 motors = [motor_left, motor_right]
 
-def configureRobotCarpet(interface):
+def configureRobot(interface):
     motorParams0 = interface.MotorAngleControllerParameters()
     motorParams0.maxRotationAcceleration = 4.0
     motorParams0.maxRotationSpeed = 10.0
-    motorParams0.feedForwardGain = 13.17
-    motorParams0.minPWM = 25
+    motorParams0.feedForwardGain = 18.5
+    motorParams0.minPWM = 23
     motorParams0.pidParameters.minOutput = -255
     motorParams0.pidParameters.maxOutput = 255
-    motorParams0.pidParameters.k_p = 700.0
-    motorParams0.pidParameters.k_i = 135.0
-    motorParams0.pidParameters.k_d = 30.0
+    motorParams0.pidParameters.k_p = 650.0
+    motorParams0.pidParameters.k_i = 240.0
+    motorParams0.pidParameters.k_d = 10.0
 
     motorParams1 = interface.MotorAngleControllerParameters()
     motorParams1.maxRotationAcceleration = 4.0
     motorParams1.maxRotationSpeed = 10.0
-    motorParams1.feedForwardGain = 13.12
-    motorParams1.minPWM = 25
+    motorParams1.feedForwardGain = 18.5
+    motorParams1.minPWM = 21
     motorParams1.pidParameters.minOutput = -255
     motorParams1.pidParameters.maxOutput = 255
-    motorParams1.pidParameters.k_p = 700.0
-    motorParams1.pidParameters.k_i = 135.0
-    motorParams1.pidParameters.k_d = 30.0
-
-    interface.motorEnable(motors[0])
-    interface.motorEnable(motors[1])
-
-    interface.setMotorAngleControllerParameters(motors[0], motorParams0)
-    interface.setMotorAngleControllerParameters(motors[1], motorParams1)
-
-def configureRobotPaper(interface):
-    motorParams0 = interface.MotorAngleControllerParameters()
-    motorParams0.maxRotationAcceleration = 4.0
-    motorParams0.maxRotationSpeed = 10.0
-    motorParams0.feedForwardGain = 13.17
-    motorParams0.minPWM = 25
-    motorParams0.pidParameters.minOutput = -255
-    motorParams0.pidParameters.maxOutput = 255
-    motorParams0.pidParameters.k_p = 570.0
-    motorParams0.pidParameters.k_i = 195.0
-    motorParams0.pidParameters.k_d = 30.0
-
-    motorParams1 = interface.MotorAngleControllerParameters()
-    motorParams1.maxRotationAcceleration = 4.0
-    motorParams1.maxRotationSpeed = 10.0
-    motorParams1.feedForwardGain = 13.12
-    motorParams1.minPWM = 25
-    motorParams1.pidParameters.minOutput = -255
-    motorParams1.pidParameters.maxOutput = 255
-    motorParams1.pidParameters.k_p = 570.0
-    motorParams1.pidParameters.k_i = 195.0
-    motorParams1.pidParameters.k_d = 30.0
+    motorParams1.pidParameters.k_p = 650.0
+    motorParams1.pidParameters.k_i = 240.0
+    motorParams1.pidParameters.k_d = 10.0
 
     interface.motorEnable(motors[0])
     interface.motorEnable(motors[1])
